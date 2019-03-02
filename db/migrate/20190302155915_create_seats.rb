351473af8f9b9f249f1c2e57008c997947b09681
Vishnu V V <vishnu@redpanthers.co>
2019-03-03 02:48:56 +0530

    Add migration for creating seats table

---
+++ db/migrate/20190302155915_create_seats.rb
@@ -0,0 +1,10 @@
+class CreateSeats < ActiveRecord::Migration[5.2]
+  def change
+    create_table :seats do |t|
+      t.belongs_to :show, index: true
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
