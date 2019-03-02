717c1ab833b43334f6626fe63cd2169b6f75f709
Vishnu V V <vishnu@redpanthers.co>
2019-03-03 04:17:20 +0530

    Add the routes for signin, signup, signout, refresh

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'refresh/create'
-  get 'signin/create'
-  get 'signup/create'
+  post 'refresh', controller: :refresh, action: :create
+  post 'signin', controller: :signin, action: :create
+  post 'signup', controller: :signup, action: :create
+  delete 'signin', controller: :signin, action: :destroy
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :shows, only: :show do
