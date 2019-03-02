2652276e2790f121c28e2f4212f8101f75714b49
Vishnu V V <vishnu@redpanthers.co>
2019-03-03 04:08:41 +0530

    Add refresh controller and its specs

---
+++ app/controllers/refresh_controller.rb
@@ -0,0 +1,14 @@
+class RefreshController < ApplicationController
+  def create
+    session = JWTSessions::Session.new(payload: claimless_payload, refresh_by_access_allowed: true)
+    tokens = session.refresh_by_access_payload do
+      raise JWTSessions::Errors::Unauthorized, 'Malicious activity detected'
+    end
+    response.set_cookie(JWTSessions.access_cookie,
+                        value: tokens[:access],
+                        httponly: true,
+                        secure: Rails.env.production?)
+
+    render json: { csrf: tokens[:csrf] }
+  end
+end
