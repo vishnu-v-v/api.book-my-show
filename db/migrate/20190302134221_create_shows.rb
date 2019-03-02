493fbd628bcf5b31ab9c688fd2b63a41bc54f4b5
Vishnu V V <vishnu@redpanthers.co>
2019-03-03 02:51:09 +0530

    Add migration for creating shows table

---
+++ db/migrate/20190302134221_create_shows.rb
@@ -0,0 +1,10 @@
+class CreateShows < ActiveRecord::Migration[5.2]
+  def change
+    create_table :shows do |t|
+      t.integer :number
+      t.string :running_in
+
+      t.timestamps
+    end
+  end
+end
