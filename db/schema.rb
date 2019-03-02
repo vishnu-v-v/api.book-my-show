2b62ba67ea6cc6fb7966246c37fb3ebeafbba821
Vishnu V V <vishnu@redpanthers.co>
2019-03-03 03:54:22 +0530

    Add users model and its spec file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_02_155915) do
+ActiveRecord::Schema.define(version: 2019_03_02_222338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,13 @@ ActiveRecord::Schema.define(version: 2019_03_02_155915) do
   create_table "shows", force: :cascade do |t|
     t.integer "number"
     t.string "running_in"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
