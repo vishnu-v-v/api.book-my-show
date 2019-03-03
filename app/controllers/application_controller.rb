23b58db5d098525f0c954cfe8733040532969c98
Vishnu V V <vishnu@redpanthers.co>
2019-03-03 22:58:00 +0530

    Rescue the not authorized requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::API
   include JWTSessions::RailsAuthorization
   rescue_from JWTSessions::Errors::Unauthorized, with: :not_authorized
+  rescue_from JWTSessions::Errors::ClaimsVerification, with: :forbidden
 
   private
 
   def current_user
     @current_user ||= User.find(payload['user_id'])
+  end
+
+  def forbidden
+    render json: { error: 'Forbidden' }, status: :forbidden
   end
 
   def not_authorized
