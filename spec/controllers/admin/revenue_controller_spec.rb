ebef2bb38dfee9c8b0dc4ff2956ae33027508101
Vishnu V V <vishnu@redpanthers.co>
2019-03-03 22:58:13 +0530

    Add spec for revenue controller

---
+++ spec/controllers/admin/revenue_controller_spec.rb
@@ -0,0 +1,12 @@
+RSpec.describe Admin::RevenueController, type: :controller do
+  let!(:user) { create(:user) }
+  let!(:admin) { create(:user, role: :admin) }
+
+  describe 'GET #show' do
+    it 'allows admin to receive total revenue' do
+      sign_in_as(admin)
+      get :show
+      expect(response).to be_successful
+    end
+  end
+end
